a2b123a744a701251c1c9affb1c948f6e68f6692
swethan34 <swetha.leader@gmail.com>
2025-05-23 17:20:11 +0530

    Inital Commit + setting up the REDDIT app variables

---
+++ crawler.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file
+load_dotenv()
+
+client_id = os.getenv("REDDIT_CLIENT_ID")
+client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+user_agent = os.getenv("REDDIT_USER_AGENT")
